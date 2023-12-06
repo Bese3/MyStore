850480e79a6513b835f56cb765ef158ee7e8788a
besufikad <07yilmabese@gmail.com>
2023-12-06 16:05:04 +0300

    final product

--- pages/main.py
+++ pages/main.py
@@ -28,7 +28,7 @@ def login():
             return redirect(url_for('home'))
     except KeyError:
         abort(401)
-    return oauth.google.authorize_redirect(redirect_uri="http://web-02.besufikadyilma.tech/authorize")
+    return oauth.google.authorize_redirect(redirect_uri="http://web-01.besufikadyilma.tech/authorize")
 
 @app.route("/home", strict_slashes=False)
 def home():
